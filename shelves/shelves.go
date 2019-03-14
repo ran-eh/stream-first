a4c49d20a5c484190b819f3d321831f779ad10eb
rehomniex <reh@omniex.io>
2019-03-13 21:27:50 -0700

    blah

--- shelves/shelves.go
+++ shelves/shelves.go
@@ -62,6 +62,14 @@ func newOverflowShelf(capacity int, temps []string) (shelf overflowShelf) {
 	return
 }
 
+func (s overflowShelf) getSlotSection(temp string) (section map[uuid.UUID]float32, err error) {
+	section, ok := s.slots[temp]
+	if !ok {
+		err = errors.Errorf("invalid temp: %v", temp)
+	}
+	return
+}
+
 func (s *overflowShelf) numOrders() int {
 	n := 0
 	for _, slotSection := range s.slots {
@@ -74,12 +82,11 @@ func (s *overflowShelf) store(orderID uuid.UUID, temp string, dacayRate float32)
 	if s.numOrders() >= s.capacity {
 		return false, nil
 	}
-	slotSection, ok := s.slots[temp]
-	if !ok {
-		// Error
-		return false, errors.Errorf("Invalid temp: %v", temp)
+	slotSection, err := s.getSlotSection(temp)
+	if err != nil {
+		return false, err
 	}
-	_, ok = slotSection[orderID]
+	_, ok := slotSection[orderID]
 	if ok {
 		return true, nil
 	}
@@ -87,30 +94,33 @@ func (s *overflowShelf) store(orderID uuid.UUID, temp string, dacayRate float32)
 	return true, nil
 }
 
-func (s *overflowShelf) remove(orderID uuid.UUID, temp string) bool {
-	slotSection, ok := s.slots[temp]
-	if !ok {
-		// Error
-		return false
+func (s *overflowShelf) remove(orderID uuid.UUID, temp string) (bool, error) {
+	slotSection, err := s.getSlotSection(temp)
+	if err != nil {
+		return false, err
 	}
-	_, ok = slotSection[orderID]
+	_, ok := slotSection[orderID]
 	if !ok {
 		// Error
-		return false
+		return false, errors.Errorf("cannot remove order %v from section %v: not in section", orderID, temp)
 	}
 	delete(slotSection, orderID)
-	return true
+	return true, nil
 }
 
-func (s *overflowShelf) popMax(temp string) (uuid.UUID, bool) {
-	found := false
-	var maxOrderID uuid.UUID
-	maxDecayRate := float32(1)
-	slotSection, ok := s.slots[temp]
-	if !ok {
-		// Error
-		return maxOrderID, found
+func (s *overflowShelf) popMax(temp string) (maxOrderID uuid.UUID, found bool, err error) {
+	maxDecayRate := float32(-1)
+
+	slotSection, err0 := s.getSlotSection(temp)
+	if err != nil {
+		err = err0
+		return
 	}
+
+	if len(slotSection) == 0 {
+		return
+	}
+
 	for orderID, decayRate := range slotSection {
 		if decayRate > maxDecayRate {
 			maxDecayRate = decayRate
@@ -120,26 +130,26 @@ func (s *overflowShelf) popMax(temp string) (uuid.UUID, bool) {
 	}
 
 	delete(slotSection, maxOrderID)
-	return maxOrderID, found
+	return
 }
 
-type pickupShelves struct {
+type warehouse struct {
 	shelves  map[string]*primaryShelf
 	overflow overflowShelf
 	ps       *pubsub.PubSub
 }
 
-func newPickupShelves(ps *pubsub.PubSub) *pickupShelves {
+func newWarehouse(ps *pubsub.PubSub) *warehouse {
 	shelves := map[string]*primaryShelf{
 		"hot":    newPrimaryShelf(15),
 		"cold":   newPrimaryShelf(15),
 		"frozen": newPrimaryShelf(15),
 	}
 	overflow := newOverflowShelf(20, []string{"hot", "cold", "frozen"})
-	return &pickupShelves{shelves, overflow, ps}
+	return &warehouse{shelves, overflow, ps}
 }
 
-func (pus *pickupShelves) store(order event.Order, temp string, Dt time.Time) bool {
+func (pus *warehouse) store(order event.Order, temp string, Dt time.Time) bool {
 	primaryShelf := pus.shelves[temp]
 	if primaryShelf == nil {
 		// Error
@@ -165,7 +175,7 @@ func (pus *pickupShelves) store(order event.Order, temp string, Dt time.Time) bo
 	return false
 }
 
-func (pus *pickupShelves) remove(orderID uuid.UUID, temp string) bool {
+func (pus *warehouse) remove(orderID uuid.UUID, temp string) bool {
 	primaryShelf := pus.shelves[temp]
 	if primaryShelf == nil {
 		// Error
@@ -175,21 +185,21 @@ func (pus *pickupShelves) remove(orderID uuid.UUID, temp string) bool {
 	if ok := primaryShelf.remove(orderID); ok {
 		return true
 	}
-	if ok := pus.overflow.remove(orderID, temp); ok {
+	if ok, _ := pus.overflow.remove(orderID, temp); ok {
 		pus.reshelf(temp)
 		return ok
 	}
 	return false
 }
 
-func (pus *pickupShelves) reshelf(temp string) bool {
+func (pus *warehouse) reshelf(temp string) bool {
 	primaryShelf := pus.shelves[temp]
 	if primaryShelf == nil {
 		// Error
 		fmt.Printf("Invalid temp: %+v\n", temp)
 		return false
 	}
-	orderID, ok := pus.overflow.popMax(temp)
+	orderID, ok, _ := pus.overflow.popMax(temp)
 	if !ok {
 		// Error
 		return false
@@ -200,7 +210,7 @@ func (pus *pickupShelves) reshelf(temp string) bool {
 
 // Run runs
 func Run(ps *pubsub.PubSub) {
-	pickupShelves := newPickupShelves(ps)
+	w := newWarehouse(ps)
 
 	newOrderCh := ps.Sub(event.EventTypeNewOrder)
 	pickUpCh := ps.Sub(event.EventTypePickup)
@@ -217,7 +227,7 @@ func Run(ps *pubsub.PubSub) {
 				fmt.Printf("could not coerce to event: shelves, new, %+v\n", msg)
 				continue
 			}
-			stored := pickupShelves.store(newOrderEvent.Order, newOrderEvent.Order.Temp, newOrderEvent.Dt)
+			stored := w.store(newOrderEvent.Order, newOrderEvent.Order.Temp, newOrderEvent.Dt)
 			if !stored {
 				wasteEvent := &event.WasteEvent{Dt: newOrderEvent.Dt, Order: newOrderEvent.Order, Reason: event.WasteReasonNoShelfSpace}
 				// fmt.Print("WS^ ")
@@ -231,7 +241,7 @@ func Run(ps *pubsub.PubSub) {
 				fmt.Printf("could not coerce to event: shelves, pickup, %+v\n", msg)
 				continue
 			}
-			pickupShelves.remove(pickupEvent.Order.ID, pickupEvent.Order.Temp)
+			w.remove(pickupEvent.Order.ID, pickupEvent.Order.Temp)
 		case msg := <-expiredCh:
 			// fmt.Print("EXv ")
 			expiredEvent, ok := msg.(*event.ExpiredEvent)
@@ -240,7 +250,7 @@ func Run(ps *pubsub.PubSub) {
 				fmt.Printf("could not coerce to event: shelves, expired, %+v\n", msg)
 				continue
 			}
-			pickupShelves.remove(expiredEvent.Order.ID, expiredEvent.Order.Temp)
+			w.remove(expiredEvent.Order.ID, expiredEvent.Order.Temp)
 		}
 	}
 }
